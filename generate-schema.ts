47d8628d1afef54b96ab0fa91302151f904a798f
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-10-23 21:26:26 +0500

    feat: allow $schema in config
    The IDE was complaining about the `$schema` property in the config file.
    This was happening because the JSON schema itself did not allow the `$schema` property.
    
    This commit updates the schema generation script to add the `$schema` property to the root of the generated JSON schema.

--- generate-schema.ts
+++ generate-schema.ts
@@ -2,7 +2,13 @@ import { writeFileSync } from 'fs';
 import { z } from 'zod';
 import { configSchema } from './config';
 
-const jsonSchema = z.toJSONSchema(configSchema);
+const jsonSchema = z.toJSONSchema(configSchema, {
+  override: (ctx) => {
+    if (ctx.zodSchema === configSchema && ctx.jsonSchema.properties) {
+      ctx.jsonSchema.properties.$schema = { type: 'string' };
+    }
+  },
+});
 
 writeFileSync('config.schema.json', JSON.stringify(jsonSchema, null, 2));
 
